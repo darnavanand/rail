8f52baf2abafc7f468e179d855652c0943ed4bab
darnavanand <78216181+darnavanand@users.noreply.github.com>
2023-06-26 16:57:37 +0530

    Add files via upload

---
+++ scripit.js
@@ -0,0 +1,30 @@
+function openPopup(){
+    document.getElementById("popup").classList.add("open-popup");
+}
+function closePopup(){
+    document.getElementById("popup").classList.remove("open-popup");
+}
+function opentelecom(){
+    document.getElementById("telecompopup").classList.add("open-popup");
+}
+function closetelecom(){
+    document.getElementById("telecompopup").classList.remove("open-popup");
+}
+function opensignal(){
+    document.getElementById("signalpopup").classList.add("open-popup");
+}
+function closesignal(){
+    document.getElementById("signalpopup").classList.remove("open-popup");
+}
+function opengear(){
+    document.getElementById("gearpopup").classList.add("open-popup");
+}
+function closegear(){
+    document.getElementById("gearpopup").classList.remove("open-popup");
+}
+function openstaff(){
+    document.getElementById("staffpopup").classList.add("open-popup");
+}
+function closestaff(){
+    document.getElementById("staffpopup").classList.remove("open-popup");
+}
